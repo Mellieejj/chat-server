3f810caf3bf692938f42f007595be2192f3fec84
Mellieejj <Melanieverheul1992@gmail.com>
2020-03-06 10:36:27 +0100

    route post

--- index.js
+++ index.js
@@ -3,4 +3,20 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+const db = {};
+
+db.messages = [];
+const parser = express.json();
+
+app.use(parser);
+
+app.post("/message", (request, response) => {
+  const { text } = request.body;
+
+  db.messages.push(text);
+
+  response.send(text);
+  console.log("db test:", db);
+});
+
 app.listen(port, () => console.log("listening on ", port));
