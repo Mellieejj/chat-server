868eb49b92f2763d2e7140ba1eb6786713ff20aa
Mellieejj <Melanieverheul1992@gmail.com>
2020-03-06 11:00:17 +0100

    stream init

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(parser);
 const stream = new Sse();
 
 app.get("/stream", (request, response) => {
+  stream.updateInit(db.message); //new user get the history
   stream.init(request, response);
 });
 
